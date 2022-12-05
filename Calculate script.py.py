0b1c496e9fe8e8e26222237c8447b2eb65c17f64
markusvh98 <92234008+markusvh98@users.noreply.github.com>
2022-12-05 11:50:56 +0100

    Add files via upload

---
+++ Calculate script.py.py
@@ -0,0 +1,18 @@
+# Create a script called CalculateVolume.py. 
+
+# The program needs to request these 3 values as inptu from the user and store them in...
+""" width = float(input("type the width:"))
+lenght = float (input("type the length:"))
+height = float (input("type the height:")) """
+
+#lengt=1.2
+#width=1.1
+#height=1.8
+# Using these 3 variable, calculate the volume of the tank and store it 
+""" Volume = lenght * width * height """
+
+
+#He requires that script increase the calculated volume.
+
+""" print(Volume * 1.01) """
+
